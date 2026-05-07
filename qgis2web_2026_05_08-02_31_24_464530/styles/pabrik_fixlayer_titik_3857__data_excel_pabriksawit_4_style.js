bfe63bee3a436d98866361a1bf25b202e0f2abcd
irmay6549-web <irmay6549@gmail.com>
2026-05-08 03:13:12 +0700

    Add files via upload

---
+++ qgis2web_2026_05_08-02_31_24_464530/styles/pabrik_fixlayer_titik_3857__data_excel_pabriksawit_4_style.js
@@ -0,0 +1,60 @@
+var size = 0;
+var placement = 'point';
+function categories_pabrik_fixlayer_titik_3857__data_excel_pabriksawit_4(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case 'BUMN':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 7.600000000000003 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(120,209,143,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Swasta':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 10.000000000000005 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(212,102,62,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+default:
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(76,28,208,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_pabrik_fixlayer_titik_3857__data_excel_pabriksawit_4 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("Kategori");
+    var labelFont = "19.5px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("Nama_pabrik") !== null) {
+        labelText = String(feature.get("Nama_pabrik"));
+    }
+    
+    var style = categories_pabrik_fixlayer_titik_3857__data_excel_pabriksawit_4(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
+
+    return style;
+};
